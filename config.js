33cd12ac9649189bb546c8a6bc3d2758de8845f1
Adminbothihi <92298372+Adminbothihi@users.noreply.github.com>
2021-12-22 23:15:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất rộng lớn như vậy mà sao lúc nào cũng gặp cậu trong tâm trí tớ vậy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Cậu thích tớ không ? ',
+    btnYes: 'Yêu luôn chứ không thích :3 <33',
+    btnNo: 'Cậu mơ hả ???',
+    question:'Trên thế giới hơn 7 tỉ người nhưng sao cậu lại thích tớ <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì tớ yêu cậu <3',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Đồng ý làm người yêu tớ nha❤.',
+    btnAccept: 'Dạaaaaaaaaa <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
